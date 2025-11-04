2d76a5222d97730f50f9f31af8187a19b293a14b
fp024 <aopenasus@naver.com>
2025-11-05 07:14:10 +0900

    길 찾기 게임 - 42892
    C# 7.0에 호환되게 맞췄다.

--- Programmers.Solutions.tests/Lv03/Exam42892Tests.cs
+++ Programmers.Solutions.tests/Lv03/Exam42892Tests.cs
@@ -7,6 +7,23 @@ public class Exam42892Tests
     [Fact]
     public void Solution_Default_Test()
     {
-        Assert.Null(new Exam42892().solution([[]]));
+        Assert.Equal(
+            new Exam42892().Solution(
+            [
+                [5, 3],
+                [11, 5],
+                [13, 3],
+                [3, 5],
+                [6, 1],
+                [1, 3],
+                [8, 6],
+                [7, 2],
+                [2, 2]
+            ])
+            , [
+                [7, 4, 6, 9, 1, 8, 5, 2, 3],
+                [9, 6, 5, 8, 1, 4, 3, 2, 7]
+            ]
+        );
     }
 }
