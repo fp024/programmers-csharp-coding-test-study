14c5f019d1b4e41c8e8584a2dbf1b3a85b7ac47f
fp024 <aopenasus@naver.com>
2025-11-10 06:01:44 +0900

    C#의 최신 문법을 사용할 수 있는 Modern 프로젝트 생성
    프로그래머스 제출용 외에, 최신 문법을 작성해볼 수 있는 문제 풀이 모던 프로젝트 생성

--- Programmers.Solutions.tests/Lv03/Exam42892Tests.cs
+++ Programmers.Solutions.tests/Lv03/Exam42892Tests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Programmers.Solutions.tests.Lv03;
 
-using Programmers.Solutions.Lv03;
+using LegacyLv03 = Programmers.Solutions.Lv03;
+using ModernLv03 = Programmers.Solutions.Modern.Lv03;
 
 public class Exam42892Tests
 {
@@ -8,7 +9,26 @@ public class Exam42892Tests
     public void Solution_Default_Test()
     {
         Assert.Equal(
-            new Exam42892().Solution(
+            new LegacyLv03.Exam42892().Solution(
+            [
+                [5, 3],
+                [11, 5],
+                [13, 3],
+                [3, 5],
+                [6, 1],
+                [1, 3],
+                [8, 6],
+                [7, 2],
+                [2, 2]
+            ])
+            , [
+                [7, 4, 6, 9, 1, 8, 5, 2, 3],
+                [9, 6, 5, 8, 1, 4, 3, 2, 7]
+            ]
+        );
+
+        Assert.Equal(
+            new ModernLv03.Exam42892().Solution(
             [
                 [5, 3],
                 [11, 5],
